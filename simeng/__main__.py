b349542ef61e93fb9081bd67235a87a5b81dce82
wstlabs <notyet@notyet.org>
2016-10-18 21:28:15 -0400

    stub class for default entry point

---
+++ simeng/__main__.py
@@ -0,0 +1,18 @@
+import sys
+import argparse
+
+"""
+    Stub class for default entry point.
+"""
+
+
+def main(args=None):
+    """The main routine."""
+
+    print("This is the main routine.")
+    print("It should do something interesting.")
+
+
+if __name__ == "__main__":
+    main()
+
