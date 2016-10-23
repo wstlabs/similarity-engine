ee6d3a8e48d1c4cbfe0e9ed57aee36122f3c3633
wstlabs <notyet@notyet.org>
2016-10-22 20:49:23 -0400

    basic sorting

--- simeng/app/flip.py
+++ simeng/app/flip.py
@@ -5,12 +5,17 @@ Flips an overlap-based like table to a normalized one.
 import sys, argparse
 import simplejson as json
 import ioany
+from collections import OrderedDict
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--infile", type=str, required=True, help="inputfile")
     parser.add_argument("--outfile", type=str, required=False, help="output file")
     return parser.parse_args()
+
+def sort_tups(seq):
+    """Nicely sorts a list of symbolic tuples, in a way we'll describe later."""
+    return sorted(seq,key=lambda k:(-len(k),k),reverse=True)[::-1]
 
 def run():
     args = parse_args()
@@ -19,6 +24,10 @@ def run():
     recs = ioany.read_csv(args.infile,types=(str,int)).recs()
     d = {r['keytup']:r['count'] for r in recs}
     print(d)
+    tups = sort_tups(d.keys())
+    print(tups)
+    pairs = [(k,d[k]) for k in tups]
+    print(pairs)
 
 if __name__ == '__main__':
     run()
