0b7aca5fd13db8b4600afd77366632c110a3a2da
wstlabs <notyet@notyet.org>
2016-10-23 16:21:08 -0400

    add active_users function

--- simeng/extras.py
+++ simeng/extras.py
@@ -1,7 +1,15 @@
 from collections import OrderedDict
 
+def active_users(eng,reverse=True):
+    """Given a SimilarityEngine instance :eng, returns a list of users sorted by like activity
+    (the number of like per user), in descending or ascending order, respectivtly, according
+    to the True/False status of the :reverse flag, which defaults to True (for descending order).
+
+    Strictly speaking it sorts on the tuple (like-activity,username) to  guarantee reproducibility."""
+    return sorted(eng.users(),key=lambda u:(len(eng.lookup(u)),u),reverse=reverse)
+
 def makereport(eng,rename=True):
-    users = sorted(eng.users(),key=lambda u:len(eng.lookup(u)),reverse=True)
+    users = active_users(eng)
     summary = [
         {
             'user':u,
